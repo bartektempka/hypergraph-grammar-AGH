ad9902ae5199ff6b6841b64b2cf2cf6dc3961e76
szymont18 <szymont181@gmail.com>
2025-12-11 21:51:43 +0100

    Implement and test Production 6

--- hypergrammar/utils.py
+++ hypergrammar/utils.py
@@ -3,6 +3,7 @@ from hypergrammar.edge import Edge
 
 
 
+
 def canonical_rotation(seq: list[str]) -> tuple[str, ...]:
     min_index = seq.index(min(seq))
     return tuple(seq[min_index:] + seq[:min_index])
